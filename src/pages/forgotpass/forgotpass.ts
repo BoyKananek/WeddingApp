23d543ba3063ef8d9879924a179e3c9805d0ad7d
Kananek Atichatviwat <kananek.ati@hotmail.com>
2016-10-31 11:46:16 +0700

    adding forgot method

--- src/pages/forgotpass/forgotpass.ts
+++ src/pages/forgotpass/forgotpass.ts
@@ -1,15 +1,66 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController,LoadingController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'forgotpass-forgotpass',
   templateUrl: 'forgotpass.html'
 })
 export class ForgotPass {
-
-  constructor(public navCtrl: NavController) {
+  email : any;
+  private disableSubmit: boolean = false;
+  constructor(public navCtrl: NavController,private http: Http,public alertCtrl: AlertController,public loadingCtrl: LoadingController) {
     
   }
+  validateEmail(email){
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+  }
+  requestToresetPass(){
+        this.disableSubmit = true;
+        if (!this.email){
+            var alert = this.alertCtrl.create({
+            title: "Sign up fail",
+            subTitle: "Please enter your email",
+            buttons: ["close"]
+            });
+            alert.present();
+            this.disableSubmit = false;
+        }
+        else if (!this.validateEmail(this.email)){
+            var alert = this.alertCtrl.create({
+            title: "Sign up fail",
+            subTitle: "Please enter your email in email format",
+            buttons: ["close"]
+            });
+            alert.present();
+            this.disableSubmit = false;
+        }
+        else{
+            var data = {
+                email : this.email
+            };
+            this.http.post("https://weddinginitiative.herokuapp.com/api/forgotPassword",data)
+            .subscribe( data =>{
+                var alert = this.alertCtrl.create({
+                title: "Reset password proceed",
+                subTitle: "Please check your email address to continue",
+                buttons: ["close"]
+            });
+                alert.present();
+                this.disableSubmit = false;
+                this.navCtrl.pop();
+            },error =>{
+                var alert = this.alertCtrl.create({
+                title: "Server down!",
+                buttons: ["close"]
+                });
+                alert.present();
+                this.disableSubmit = false;
+            });
+        }
+    }
 
 }
